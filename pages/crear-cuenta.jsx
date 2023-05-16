f0e92451a90f2b0e123d49299919e5a003b2cb7d
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-05-16 10:37:10 -0500

    Nice

--- pages/crear-cuenta.jsx
+++ pages/crear-cuenta.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario';
 
 const CrarCuenta = () => (
   <div>
     <Layout>
-      <h1>Crar Cuenta</h1>
+      <>
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >
+          Crar Cuenta
+        </h1>
+
+        <Formulario>
+          <Campo>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Tu Nombre"
+              name="nombre"
+            />
+          </Campo>
+
+          <Campo>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Tu Email"
+              name="email"
+            />
+          </Campo>
+
+          <Campo>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Tu Password"
+              name="password"
+            />
+          </Campo>
+
+          <InputSubmit type="submit" value="Crear Cuenta" />
+        </Formulario>
+      </>
     </Layout>
   </div>
 );
