eb0c096e988fba63c647339adf956e05a14456d5
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-01 12:48:56 -0500

    Brutal.
    Ahora si esta funcionando en forma.

---
+++ components/layout/DetallesProducto.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale';
+
+const Producto = styled.li`
+  padding: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+const DescriptionProducto = styled.div`
+  flex: 0 1 600px;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  column-gap: 2rem;
+`;
+
+const Titulo = styled.a`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const TextoDescripcion = styled.p`
+  font-size: 1.6rem;
+  margin: 0;
+  color: #888;
+`;
+
+const Comentarios = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    border: 1px solid #e1e1e1;
+    padding: 0.3rem 1rem;
+    margin-right: 2rem;
+  }
+  img {
+    width: 2rem;
+    margin-right: 2rem;
+  }
+  p {
+    font-size: 1.6rem;
+    margin-right: 1rem;
+    font-weight: 700;
+    &:last-of-type {
+      margin: 0;
+    }
+  }
+`;
+
+const Imagen = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+const Votes = styled.div`
+  flex: 0 0 auto;
+  text-align: center;
+  border: 1px solid #e1e1e1;
+  padding: 1rem 3rem;
+  div {
+    font-size: 2rem;
+  }
+  p {
+    margin: 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+`;
+
+export default function DetallesProducto({ producto }) {
+  const {
+    id,
+    comentarios,
+    creado,
+    descripcion,
+    empresa,
+    nombre,
+    url,
+    urlImagen,
+    votos,
+  } = producto;
+
+  return (
+    <Producto>
+      <DescriptionProducto>
+        <div>
+          <Imagen src={urlImagen} />
+        </div>
+        <div>
+          <Titulo>{nombre}</Titulo>
+          <TextoDescripcion>{descripcion}</TextoDescripcion>
+          <div>
+            <Comentarios>
+              <img src="/static/img/comentario.png" />
+              <p>{comentarios.length} Comentarios</p>
+            </Comentarios>
+            <p>
+              Puclicado hace:{' '}
+              {formatDistanceToNow(new Date(creado), { locale: es })}
+            </p>
+          </div>
+        </div>
+      </DescriptionProducto>
+      <Votes>
+        <div>&#9650;</div>
+        <p>{votos}</p>
+      </Votes>
+    </Producto>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { collection, getDocs, orderBy } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import DetallesProducto from '../components/layout/DetallesProducto';
 
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
-  </div>
-);
+const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+
+  
+
+  useEffect(() => {
+    const obtenerProductos = async () => {
+      const querySnapshot = await getDocs(
+        collection(db, 'productos'),
+        orderBy('creado', 'desc')
+      );
+      const productos = querySnapshot.docs.map((doc) => doc.data());
+      guardarProductos(productos);
+    };
+    obtenerProductos();
+  }, []);
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetallesProducto 
+                  key={producto.id}
+                  producto={producto}
+                
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+};
 
 export default Home;
