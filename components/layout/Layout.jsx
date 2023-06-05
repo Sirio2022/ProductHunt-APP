1f5d142c5716b7acfc8264dd362fc9e911ff5be1
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-05 13:00:40 -0500

    Va al pelo

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -61,13 +61,12 @@ export default function Layout(props) {
       />
 
       <Head>
-        <html lang="es" />
         <title>Product Hunt Firebase y Next.js</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
           integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
--- pages/productos/[id].jsx
+++ pages/productos/[id].jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { useRouter as useRouter2 } from 'next/navigation';
 import Link from 'next/link';
 import Layout from '../../components/layout/Layout';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
 import Error404 from '../../components/layout/404';
 import { css } from '@emotion/react';
@@ -13,6 +13,7 @@ import { es } from 'date-fns/locale';
 import { Campo, InputSubmit } from '../../components/ui/Formulario';
 import Boton from '../../components/ui/Boton';
 import { FirebaseContext } from '../../firebase';
+import { set } from 'date-fns';
 
 const ContenedorProducto = styled.div`
   @media (min-width: 768px) {
@@ -37,6 +38,7 @@ export default function Producto() {
   const [producto, setProducto] = useState({});
   const [error, setError] = useState(false);
   const [comentario, setComentario] = useState({});
+  const [consultarDB, setConsultarDB] = useState(true);
 
   // Routing para obtener el id actual
   const router = useRouter();
@@ -50,25 +52,27 @@ export default function Producto() {
   const { usuario } = useContext(FirebaseContext);
 
   useEffect(() => {
-    if (id) {
+    if (id && consultarDB) {
       const obtenerProducto = async () => {
         const docRef = doc(db, 'productos', id);
         await getDoc(docRef).then((doc) => {
           if (doc.exists()) {
             setProducto(doc.data());
+            setConsultarDB(false);
             //console.log('Document data:', doc.data());
           } else {
             // doc.data() will be undefined in this case
             setError(true);
+            setConsultarDB(false);
           }
         });
       };
 
       obtenerProducto();
     }
-  }, [id, producto]);
+  }, [id]);
 
-  if (Object.keys(producto).length === 0) return 'Cargando...';
+  if (Object.keys(producto).length === 0 && !error) return 'Cargando...';
 
   const {
     comentarios,
@@ -111,6 +115,7 @@ export default function Producto() {
       ...producto,
       votos: nuevoTotal,
     });
+    setConsultarDB(true); // Hay un voto, por lo tanto consultar a la BD
   };
 
   // Funciones para crear comentarios
@@ -153,113 +158,149 @@ export default function Producto() {
       ...producto,
       comentarios: nuevosComentarios,
     });
+    setConsultarDB(true); // Hay un comentario, por lo tanto consultar a la BD
+    setComentario({}); // Reiniciar el form
+  };
+
+  // Funcion que revisa que el creador del producto sea el mismo que esta autenticado
+  const puedeBorrar = () => {
+    if (!usuario) return false;
+
+    if (creador.id === usuario.uid) {
+      return true;
+    }
+  };
+
+  // Elimina un producto de la BD
+  const eliminarProducto = async () => {
+    if (!usuario) {
+      return router2.push('/login');
+    }
+
+    if (creador.id !== usuario.uid) {
+      return router2.push('/');
+    }
+    try {
+      const docRef = doc(db, 'productos', id);
+      await deleteDoc(docRef);
+      return router2.push('/');
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
     <Layout>
       <>
-        {error && <Error404 />}
-        <div className="contenedor">
-          <h1
-            css={css`
-              text-align: center;
-              margin-top: 5rem;
-            `}
-          >
-            {nombre}
-          </h1>
-          <ContenedorProducto>
-            <div>
-              <p>
-                Puclicado hace:{' '}
-                {formatDistanceToNow(new Date(creado), { locale: es })}
-              </p>
-              <p>
-                Por {creador.nombre}, de la empresa: {empresa}
-              </p>
-
-              <img src={urlImagen} />
-              <p>{descripcion}</p>
-
-              {usuario && (
-                <>
-                  <h2>Agrega tu comentario</h2>
-                  <form onSubmit={agregarComentario}>
-                    <Campo>
-                      <input
-                        type="text"
-                        name="mensaje"
-                        onChange={comentarioChange}
-                      />
-                    </Campo>
-                    <InputSubmit type="submit" value="Agregar comentario" />
-                  </form>
-                </>
-              )}
-
-              <h2
-                css={css`
-                  margin: 2rem 0;
-                `}
-              >
-                Comentarios
-              </h2>
-
-              {comentarios.length === 0 ? (
-                'Aún non hay comentarios'
-              ) : (
-                <ul>
-                  {comentarios.map((comentario, i) => (
-                    <li
-                      key={`${comentario.usuarioId}-${i}`}
-                      css={css`
-                        border: 1px solid #e1e1e1;
-                        padding: 2rem;
-                      `}
-                    >
-                      <p>{comentario.mensaje}</p>
-                      <p>
-                        Escrito por:
-                        <span
-                          css={css`
-                            font-weight: bold;
-                          `}
-                        >
-                          {' '}
-                          {comentario.usuarioNombre}
-                        </span>
-                      </p>
-                      {esCreardor(comentario.usuarioId) && (
-                        <CreadorProducto> Es Creador </CreadorProducto>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-            <aside>
-              <Link href={url} passHref target="_blank">
-                <Boton bgColor="true">Visitar URL</Boton>
-              </Link>
-
-              <div
-                css={css`
-                  margin-top: 5rem;
-                `}
-              >
-                <p
-                  css={css`
-                    text-align: center;
-                  `}
-                >
-                  {votos} Votos
+        {error ? (
+          <Error404 />
+        ) : (
+          <div className="contenedor">
+            <h1
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+              `}
+            >
+              {nombre}
+            </h1>
+            <ContenedorProducto>
+              <div>
+                <p>
+                  Puclicado hace:{' '}
+                  {formatDistanceToNow(new Date(creado), { locale: es })}
+                </p>
+                <p>
+                  Por {creador.nombre}, de la empresa: {empresa}
                 </p>
 
-                {usuario && <Boton onClick={votarProducto}>Votar</Boton>}
+                <img src={urlImagen} />
+                <p>{descripcion}</p>
+
+                {usuario && (
+                  <>
+                    <h2>Agrega tu comentario</h2>
+                    <form onSubmit={agregarComentario}>
+                      <Campo>
+                        <input
+                          type="text"
+                          name="mensaje"
+                          onChange={comentarioChange}
+                        />
+                      </Campo>
+                      <InputSubmit type="submit" value="Agregar comentario" />
+                    </form>
+                  </>
+                )}
+
+                <h2
+                  css={css`
+                    margin: 2rem 0;
+                  `}
+                >
+                  Comentarios
+                </h2>
+
+                {comentarios.length === 0 ? (
+                  'Aún non hay comentarios'
+                ) : (
+                  <ul>
+                    {comentarios.map((comentario, i) => (
+                      <li
+                        key={`${comentario.usuarioId}-${i}`}
+                        css={css`
+                          border: 1px solid #e1e1e1;
+                          padding: 2rem;
+                        `}
+                      >
+                        <p>{comentario.mensaje}</p>
+                        <p>
+                          Escrito por:
+                          <span
+                            css={css`
+                              font-weight: bold;
+                            `}
+                          >
+                            {' '}
+                            {comentario.usuarioNombre}
+                          </span>
+                        </p>
+                        {esCreardor(comentario.usuarioId) && (
+                          <CreadorProducto> Es Creador </CreadorProducto>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
-            </aside>
-          </ContenedorProducto>
-        </div>
+              <aside>
+                <Link href={url} passHref target="_blank">
+                  <Boton bgColor="true">Visitar URL</Boton>
+                </Link>
+
+                <div
+                  css={css`
+                    margin-top: 5rem;
+                  `}
+                >
+                  <p
+                    css={css`
+                      text-align: center;
+                    `}
+                  >
+                    {votos} Votos
+                  </p>
+
+                  {usuario && <Boton onClick={votarProducto}>Votar</Boton>}
+                </div>
+              </aside>
+            </ContenedorProducto>
+
+            {puedeBorrar() && (
+              <Boton onClick={eliminarProducto}>Eliminar Producto</Boton>
+            )}
+          </div>
+        )}
       </>
     </Layout>
   );
